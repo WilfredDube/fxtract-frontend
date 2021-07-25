c9fb24e78b63fcb3148d1e3ab1d426e44d4f31ae
unbusy <unbusy@lhost.com>
2021-07-25 13:12:34 +0200

    Add 3D viewer for CAD models

---
+++ src/components/cad-viewer/ViewerCanvas.js
@@ -0,0 +1,28 @@
+import * as THREE from "three";
+import React, { Suspense } from "react";
+import { Canvas, useLoader } from "@react-three/fiber";
+// import { Stage } from "@react-three/drei";
+import { MTLLoader, OBJLoader, DDSLoader } from "three-stdlib";
+
+THREE.DefaultLoadingManager.addHandler(/\.dds$/i, new DDSLoader());
+
+function Model(props) {
+  const materials = useLoader(MTLLoader, "../../../test/90.mtl");
+  const obj = useLoader(OBJLoader, "../../../test/90.obj", (loader) => {
+    materials.preload();
+    loader.setMaterials(materials);
+  });
+  return <primitive object={obj} {...props} />;
+}
+
+export default function ViewerCanvas() {
+  return (
+    <Canvas camera={{ position: [0, 0, 180], fov: 60 }}>
+      <Suspense fallback={null}>
+        {/* <Stage> */}
+        <Model />
+        {/* </Stage> */}
+      </Suspense>
+    </Canvas>
+  );
+}
