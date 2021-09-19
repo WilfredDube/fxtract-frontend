b4be2d202ff9ebb7d84d402e3e0c9d15f270919f
unbusy <unbusy@lhost.com>
2021-09-19 17:11:48 +0200

    Clean up

--- src/state/DataStore.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from "redux";
-import { ProjectReducer } from "./ProjectReducer";
-
-export const FxtractDataStore = createStore(
-  ProjectReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
