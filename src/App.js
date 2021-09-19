dfa9508a8cecf190c4473cfc3da7caf27421adf0
unbusy <unbusy@lhost.com>
2021-09-19 16:51:24 +0200

    Update changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 // import logo from "./logo.svg";
-import "./App.css";
+// import "./App.css";
 
-import { FxtractDataStore } from "./state/DataStore";
-import { Provider } from "react-redux";
 import {
   BrowserRouter as Router,
   Route,
   Switch,
   Redirect,
 } from "react-router-dom";
-import { ProjectConnector } from "./components/project/ProjectConnector";
 import { makeStyles } from "@material-ui/core";
 import SignIn from "./components/view/SignIn";
 import SignUp from "./components/view/SignUp";
 import ForgotPassword from "./components/view/ForgotPassword";
 import VerifyEmail from "./components/view/VerifyEmail";
 import ProjectViewer from "./components/cad-viewer/ProjectViewer";
+import ProjectContextProvider from "./contexts/ProjectContext";
+import ProjectListView from "./components/project/ProjectListView";
+import ResetPassword from "./components/view/ResetPassword";
+import AuthContextProvider from "./contexts/AuthContext";
 
 const useStyles = makeStyles((theme) => ({
   toolbarMargin: theme.mixins.toolbar,
@@ -24,20 +25,25 @@ const useStyles = makeStyles((theme) => ({
 const App = () => {
   const classes = useStyles();
   return (
-    <Provider store={FxtractDataStore}>
+    // <Provider store={FxtractDataStore}>
+    <AuthContextProvider>
       <Router>
         <div className={classes.toolbarMargin} />
         <Switch>
-          <Route path="/" component={SignIn} exact />
+          <Route path="/signin" component={SignIn} exact />
           <Route path="/signup" component={SignUp} exact />
           <Route path="/forgot-password" component={ForgotPassword} exact />
           <Route path="/verify-email" component={VerifyEmail} exact />
-          <Route path="/projects" component={ProjectConnector} exact />
-          <Route path="/view" component={ProjectViewer} exact />
-          <Redirect to="/" />
+          <Route path="/reset-password" component={ResetPassword} exact />
+          <ProjectContextProvider>
+            <Route path="/projects" component={ProjectListView} exact />
+            <Route path="/view" component={ProjectViewer} exact />
+          </ProjectContextProvider>
+          <Redirect to="/signin" />
         </Switch>
       </Router>
-    </Provider>
+    </AuthContextProvider>
+    // </Provider>
   );
 };
 
